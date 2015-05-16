20987996c1996468f555e7b1f042f229b3ce831d
legshooter <legshooter@users.noreply.github.com>
2015-05-16 21:33:20 +0300

    Create addel.js

---
+++ addel.js
@@ -0,0 +1,82 @@
+(function ( $ ) {
+
+    $.fn.addel = function( options ) {
+
+        'use strict';
+
+        var classSymbol = '.';
+
+        var defaults = {
+            target: 'addel-target',
+            add: 'addel-add',
+            del: 'addel-del',
+            delAlert: 'למחוק?'
+        };
+
+        var settings = $.extend( true, {}, defaults, options );
+
+        var target = classSymbol + settings.target;
+        var add = classSymbol + settings.add;
+        var del = classSymbol + settings.del;
+        var formElements = 'input, select, textarea';
+
+        return this.each(function() {
+
+            var container = $(this);
+
+            // init
+            container.find(target).hide().find(formElements).prop('disabled', true);
+
+            // add
+            container.on('click', add, function() {
+
+                var targetToAdd = container.find(target).last();
+
+                // no visible targets
+                if (targetToAdd.filter(':visible').length === 0) {
+                    targetToAdd.fadeIn().find(formElements).prop('disabled', false);
+
+                // target/s visible
+                } else {
+                    targetToAdd.clone(true).insertAfter(targetToAdd).hide().fadeIn();
+                }
+
+                container.find(target).last().find(':input:enabled:visible:first').focus();
+
+            });
+
+            // del
+            container.on('click', del, function() {
+
+                if (confirm(settings.delAlert)) {
+
+                    var targetToDel = $(this).closest(target);
+                    var targetToDelPrev = targetToDel.prev(target);
+                    var targetToDelNext = targetToDel.next(target);
+
+                    // 1 target exists
+                    if (container.find(target).length === 1) {
+
+                        targetToDel.fadeOut().find(formElements).prop('disabled', true);
+                        container.find(add).focus();
+
+                    // >1 targets exist
+                    } else {
+
+                        targetToDel.fadeOut(function() {$(this).remove()});
+
+                        if (targetToDelPrev.length === 1) {
+                            targetToDelPrev.find(del).focus();
+                        } else {
+                            targetToDelNext.find(del).focus();
+                        }
+                    }
+
+                }
+
+            });
+
+        });
+    };
+
+}( jQuery ));
